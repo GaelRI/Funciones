9f9c13f7ac8a38221dd805f65d945dcbe71c877a
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-19 10:33:30 -0600

    Update Funcion1.m

--- Funcion1.m
+++ Funcion1.m
@@ -1,11 +1,11 @@
 % octave.scriptFuncio
 % Escuela: Tecnologico de Estudios Superiores de Jilotepec
-% Especialidad: Ingeniería en Sistemas Computacionales
+% Especialidad: IngenierÃ­a en Sistemas Computacionales
 % Titulo:Funciones Algebraicas:polinomiales y racionales
-% Descripción:Realizar las funciones y Escribir el tipo de funcion algebraica  
-% Autor:Gael Franco García
+% DescripciÃ³n:Realizar las funciones y Escribir el tipo de funcion algebraica  
+% Autor:Gael Franco GarcÃ­a
 % Fecha:17-11-2021
-% Versión: 1
+% VersiÃ³n: 1
 % Funcion 1.1
 
 
@@ -21,3 +21,4 @@ plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([0 0],[-30 30],'m-',"linewidth",2,"markersize",8);
 title(['fx=(x+2)(x-2) Funcion 2-1 Polinomica']);
 disp('Como podemos observar, se grafica de manera continua lo que la vuelve polinomica');
+disp('Los puntos donde f(x)=0 son: (-2,0) y (2,0)');
