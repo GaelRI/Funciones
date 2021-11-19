4a8b6b51149f5fe6571324ee93a7e51ea8ac5c5a
GaelRI <91554106+GaelRI@users.noreply.github.com>
2021-11-19 10:34:32 -0600

    Update Funcion2.m

--- Funcion2.m
+++ Funcion2.m
@@ -1,11 +1,11 @@
 % octave.scriptFuncios
 % Escuela: Tecnologico de Estudios Superiores de Jilotepec
-% Especialidad: Ingeniería en Sistemas Computacionales
+% Especialidad: IngenierÃ­a en Sistemas Computacionales
 % Titulo:Funciones Algebraicas:polinomiales y racionales
-% Descripción:Realizar las funciones y Escribir el tipo de funcion algebraica
-% Autor:Gael Franco García
+% DescripciÃ³n:Realizar las funciones y Escribir el tipo de funcion algebraica
+% Autor:Gael Franco GarcÃ­a
 % Fecha:18-11-2021
-% Versión: 1
+% VersiÃ³n: 1
 % Funcion 2.2
 
 clear 
@@ -20,3 +20,4 @@ plot([-20 20],[0 0],'m-',"linewidth",2,"markersize",8);
 plot([0 0],[-30 30],'m-',"linewidth",2,"markersize",8);
 title([' fx=x+2/x-1 Funcion 2.2 Racional']);
 disp('La funcion es Racional: Pues la funcion tiene discontinuidades en sus puntos');
+disp('El punto donde f(x)=0 es: (-2,0)');
